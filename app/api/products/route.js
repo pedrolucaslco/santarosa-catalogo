c6906f29f8406d247bf20b949418786816cdf7aa
Pedro Lucas <pedrolucaslcosta@gmail.com>
2024-05-30 17:33:47 -0300

    API

---
+++ app/api/products/route.js
@@ -0,0 +1,46 @@
+// app/api/products/route.js
+import fs from "fs";
+import path from "path";
+
+export async function GET(request) {
+  const productsDirectory = path.join(process.cwd(), "public/products");
+
+  try {
+    // Lê os arquivos na pasta especificada
+    const filenames = fs.readdirSync(productsDirectory);
+
+    // Processa cada nome de arquivo
+    const products = filenames
+      .map((filename, index) => {
+        const regex = /(.*) R\$ (\d+,\d+)/;
+        const match = filename.match(regex);
+
+        if (match) {
+          var productName = match[1].trim();
+		  const parts = productName.split(". ");
+		  productName = parts[1];
+          const price = match[2];
+          const productUrl = `/products/${encodeURIComponent(filename)}`; // URL do arquivo
+          return {
+            id: index + 1,
+            name: productName,
+            price: price,
+            url: productUrl,
+          };
+        }
+
+        return null;
+      })
+      .filter((product) => product !== null);
+
+    return new Response(JSON.stringify(products), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "Failed to read products" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,48 +3,59 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
 
 	const [searchTerm, setSearchTerm] = useState('');
 
-	const productsArray = [
-		{ id: 1, name: 'Card 1', price: 149.90},
-		{ id: 2, name: 'Card 2', price: 149.90},
-		{ id: 3, name: 'Card 3', price: 149.90},
-		{ id: 4, name: 'Card 4', price: 149.90},
-		{ id: 5, name: 'Card 5', price: 149.90},
-		{ id: 6, name: 'Card 6', price: 149.90},
-		{ id: 7, name: 'Card 7', price: 149.90},
-		{ id: 8, name: 'Card 8', price: 149.90},
-		{ id: 9, name: 'Card 9', price: 149.90},
-		{ id: 10, name: 'Card 10', price: 149.90},
-		{ id: 11, name: 'Card 11', price: 149.90},
-		{ id: 12, name: 'Card 12', price: 149.90},
-	];
+	const [products, setProducts] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
 
-	const filteredProducts = productsArray.filter((product) =>
+	useEffect(() => {
+		const fetchProducts = async () => {
+			try {
+				const response = await fetch('/api/products');
+				if (!response.ok) {
+					throw new Error('Network response was not ok');
+				}
+				const data = await response.json();
+				setProducts(data);
+			} catch (err) {
+				setError(err.message);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchProducts();
+	}, []);
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error: {error}</p>;
+
+	const filteredProducts = products.filter((product) =>
 		product.name.toLowerCase().includes(searchTerm.toLowerCase())
 	);
 
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-start w-screen py-8 px-4 sm:px-8 md:px-10 lg:px-20">
 			<div className="w-full sm:w-full md:w-80 lg:w-80 pb-8">
-				<Input 	type="text"
-        				placeholder="Pesquisar"
-        				value={searchTerm}
-        				onChange={(e) => setSearchTerm(e.target.value)}>
+				<Input type="text"
+					placeholder="Pesquisar"
+					value={searchTerm}
+					onChange={(e) => setSearchTerm(e.target.value)}>
 				</Input>
 			</div>
 			<div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-4 w-full">
 				{filteredProducts.map((product) => (
 					<Card className="overflow-hidden ">
-						<Image src="/product-blank.png" alt="Product Image" width={500} height={400} className="w-full h-64 object-cover pb-4" />
+						<Image src={product.url} alt="Product Image" width={500} height={400} className="w-full h-64 object-cover pb-4" loading="lazy" />
 						<CardContent>
 							<div className="flex justify-start flex-wrap flex-col">
 								<h3 className="text-xl font-bold mb-2">{product.name}</h3>
-								<p className="text-gray-700 dark:text-gray-400 text-lg font-semibold">{product.price}</p>
+								<p className="text-gray-700 dark:text-gray-400 text-lg font-semibold">R${product.price}</p>
 							</div>
 						</CardContent>
 					</Card>
